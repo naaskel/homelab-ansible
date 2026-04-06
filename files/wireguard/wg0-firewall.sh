ee8f58da9105aba3183d48a9edf793d6d4d28497
naaskel <45149007+naaskel@users.noreply.github.com>
2026-04-06 23:56:13 +0200

    adguard

--- files/wireguard/wg0-firewall.sh
+++ files/wireguard/wg0-firewall.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 if [ "$1" == "up" ]; then
-    nft delete table inet wireguard 2>/dev/null
-    nft delete table ip nat 2>/dev/null
-
-    nft add table inet wireguard
-    nft add chain inet wireguard forward '{ type filter hook forward priority 0; policy accept; }'
-    nft add rule inet wireguard forward iifname "wg0" accept
-    nft add rule inet wireguard forward oifname "wg0" accept
-
-    nft add table ip nat
-    nft add chain ip nat postrouting '{ type nat hook postrouting priority 100; }'
+    nft add chain ip filter DOCKER-USER
+    nft insert rule ip filter DOCKER-USER iifname "wg0" accept
+    nft insert rule ip filter DOCKER-USER oifname "wg0" accept
     nft add rule ip nat postrouting oifname "eth0" masquerade
 
 elif [ "$1" == "down" ]; then
-    nft delete table inet wireguard 2>/dev/null
-    nft delete table ip nat 2>/dev/null
+    nft flush chain ip filter DOCKER-USER
+    nft delete rule ip nat postrouting handle \
+        $(nft -a list chain ip nat postrouting | grep 'oifname "eth0" masquerade' | awk '{print $NF}') 2>/dev/null
 fi
