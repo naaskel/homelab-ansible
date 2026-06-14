d37307876e73625e7c13e555805f3059be6229b0
naaskel <45149007+naaskel@users.noreply.github.com>
2026-06-14 02:25:26 +0200

    nftables tables setup + ssh drop-in hardening

--- files/wireguard/wg0-firewall.sh
+++ files/wireguard/wg0-firewall.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 if [ "$1" == "up" ]; then
-    nft add chain ip filter DOCKER-USER
+    nft add table ip filter
+    nft 'add chain ip filter DOCKER-USER { type filter hook forward priority 0; }'
     nft insert rule ip filter DOCKER-USER iifname "wg0" accept
     nft insert rule ip filter DOCKER-USER oifname "wg0" accept
+
+    nft add table ip nat
+    nft 'add chain ip nat postrouting { type nat hook postrouting priority 100; }'
     nft add rule ip nat postrouting oifname "eth0" masquerade
 
 elif [ "$1" == "down" ]; then
-    nft flush chain ip filter DOCKER-USER
+    nft flush chain ip filter DOCKER-USER 2>/dev/null
     nft delete rule ip nat postrouting handle \
-        $(nft -a list chain ip nat postrouting | grep 'oifname "eth0" masquerade' | awk '{print $NF}') 2>/dev/null
+        $(nft -a list chain ip nat postrouting 2>/dev/null | grep 'oifname "eth0" masquerade' | awk '{print $NF}') 2>/dev/null
 fi
